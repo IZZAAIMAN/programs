217d1a8f30614f85fa30f7e504f3a2e3bd23ef60
IZZAAIMAN <125851397+IZZAAIMAN@users.noreply.github.com>
2023-02-21 19:19:20 -0800

    Add files via upload

---
+++ maitiplication of table.cpp
@@ -0,0 +1,15 @@
+//c++program to print multiplication table of a given number?
+#include<iostream>
+using namespace std;
+int main()
+{
+	int x,y;
+	cout<<"enter the number"<<endl;
+	cin>>x;
+	for(y=1;y<=10;y++)
+	{
+		cout<<x<<"*"<<y<<"="<<x*y<<endl;
+			}
+			return 0;
+}
+
