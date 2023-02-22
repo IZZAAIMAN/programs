9369a9be21920dea167c6be54bb434d18c6048cb
IZZAAIMAN <125851397+IZZAAIMAN@users.noreply.github.com>
2023-02-21 19:17:37 -0800

    Add files via upload

---
+++ find the sum of first and last digit of any number.cpp
@@ -0,0 +1,23 @@
+//c++ program to find the sum of first and last digits of any number?
+#include<iostream>
+using namespace std;
+int main()
+{
+   int num, temp=0, last, rem, sum=0;
+   cout<<"Enter a Number: ";
+   cin>>num;
+   while(num!=0)
+   {
+      if(temp==0)
+      {
+         last = num%10;
+         temp++;
+      }
+      rem = num%10;
+      num = num/10;
+   }
+   sum = rem + last;
+   cout<<"\nSum of First ("<<rem<<") and Last ("<<last<<") Digit = "<<sum;
+   cout<<endl;
+   return 0;
+}
