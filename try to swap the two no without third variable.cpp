5c94a1135fe8a193caf88f69e3c7cad1667ef44a
IZZAAIMAN <125851397+IZZAAIMAN@users.noreply.github.com>
2023-02-21 19:21:03 -0800

    Add files via upload

---
+++ try to swap the two no without third variable.cpp
@@ -0,0 +1,14 @@
+//Try to swap the two variable without thiord variable?
+ #include <iostream>  
+using namespace std;  
+int main()  
+{  
+int a=5, b=10;      
+cout<<"Before swap a= "<<a<<" b= "<<b<<endl;      
+a=a*b;     
+b=a/b;     
+a=a/b;    
+cout<<"After swap a= "<<a<<" b= "<<b<<endl;      
+return 0;  
+}  
+
