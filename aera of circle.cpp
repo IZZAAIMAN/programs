b7d42e68db287f3a15f437d95faa01de8aa54ad2
IZZAAIMAN <125851397+IZZAAIMAN@users.noreply.github.com>
2023-02-21 19:34:43 -0800

    Add files via upload

---
+++ aera of circle.cpp
@@ -0,0 +1,12 @@
+// write a program to calculate Area of circle.
+#include<iostream>
+using namespace std;
+int main ()
+{
+float radius ,area ;
+cout<<"enter the radius of circle";
+cin>>radius ;
+area =3.14*radius *radius ;
+cout<<"area of circle with radius "<<radius <<" is "<<area ;
+return 0;	
+}
