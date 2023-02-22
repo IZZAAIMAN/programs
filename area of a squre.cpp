7a58f03dc47b3cc7642ac5b5cc65d59d8d14e3ea
IZZAAIMAN <125851397+IZZAAIMAN@users.noreply.github.com>
2023-02-21 19:35:24 -0800

    Add files via upload

---
+++ area of a squre.cpp
@@ -0,0 +1,12 @@
+//c++program to calculate area of squre?
+#include<iostream >
+using namespace std;
+int main ()
+{
+	int side ,area ;
+	cout<<"enter the side of a squre :";
+	cin>>side ;
+	area =side *side ;
+	cout<<"area of a squre is :"<<area ;
+	return 0 ;
+}
